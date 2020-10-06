f8cd727819b94e16ec2bd8fc525eed3683f201b3
Islam <islam_tramov@mail.ru>
2020-10-06 21:24:25 +0300

    Done socket connection version. Sending commands to client.

--- Server/src/com/company/Server.java
+++ Server/src/com/company/Server.java
@@ -7,8 +7,9 @@ public class Server {
 
     private static Socket clientSocket;
     private static ServerSocket server;
-    private static BufferedReader in;   // Socket read stream
-    private static BufferedWriter out;  // Socket write stream
+    private static BufferedReader command_reader;   // Reads commands from terminal
+    private static BufferedReader in;               // Socket read stream
+    private static BufferedWriter out;              // Socket write stream
 
 
     public static void init_server() {
@@ -17,20 +18,24 @@ public class Server {
                 server = new ServerSocket(9001);
                 System.out.println("Server started listening");
                 clientSocket = server.accept();
-
                 try {
+                    command_reader = new BufferedReader(new InputStreamReader(System.in));
+
                     // Recieve messages
                     in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
                     // Send messages
                     out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
 
-                    String client_message = in.readLine();
-                    System.out.println(client_message);
-
-                    // Write answer to client
-                    out.write("OK");
+                    // Send server command
+                    String command = command_reader.readLine();
+                    out.write(command);
                     out.newLine();
                     out.flush();
+
+                    // Read indications recieved from client
+                    String client_message = in.readLine();
+                    System.out.println(client_message);
 
                 } finally {
                     // Closing socket
@@ -39,7 +44,6 @@ public class Server {
                     out.close();
                 }
             } finally {
-                System.out.println("Server closed");
                 server.close();
             }
 
