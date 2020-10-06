2fe1cced2d3929bafe956f235177364cccef7b01
Islam <islam_tramov@mail.ru>
2020-10-06 23:10:25 +0300

    Server RMI support release.

--- Server/src/com/company/Main.java
+++ Server/src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 import com.company.Server;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RemoteException, NotBoundException {
         Server server_socket = new Server();
         server_socket.init_server();
     }
